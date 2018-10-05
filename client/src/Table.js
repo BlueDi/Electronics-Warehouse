d369abcb0a10717f3937879e5795b57110faa9e4
Diogo Cruz <diogohmcruz@hotmail.com>
2018-10-05 14:10:45 +0100

    Table receives data from Server
    The modifications in server.js are for testing.
    If failed to fetch, a loading is shown.

--- client/src/Table.js
+++ client/src/Table.js
@@ -1,91 +1,127 @@
 import React, { Component } from "react";
-import { Icon, Label, Menu, Table } from "semantic-ui-react";
+import { Dimmer, Icon, Loader, Menu, Table } from "semantic-ui-react";
+
+const urlForData = id => `/api/table/${id}`;
 
 class TableExample extends Component {
   constructor(props) {
     super(props);
-    this.list = [
-      {
-        id: 0,
-        title: "Project1",
-        description: "hueee",
-        location: "ali",
-        earned: "",
-        target: "",
-        end_date: ""
-      },
-      {
-        id: 1,
-        title: "Project2",
-        description: "",
-        location: "",
-        earned: "",
-        target: "",
-        end_date: ""
-      }
-    ];
-    this.state = {};
-    this.table_body = {};
+    this.state = {
+      isFetching: true,
+      isError: false,
+      components: [
+        {
+          code: 0,
+          number: "Project1",
+          description: "hueee",
+          location: "ali",
+          availability: 1,
+          price: 21.29
+        },
+        {
+          id: 1,
+          number: "Project2",
+          description: "",
+          location: "",
+          availability: 0,
+          price: 0
+        }
+      ]
+    };
   }
 
-  table_cell = i => [
-    <Table.Cell key={i}>
-      <Label ribbon>{i}</Label>
-    </Table.Cell>
-  ];
+  componentDidMount() {
+    fetch(urlForData(this.props.match.params.id))
+      .then(response => {
+        if (!response.ok) {
+          throw Error("Network request failed");
+        }
+        return response;
+      })
+      .then(result => result.json())
+      .then(
+        result => {
+          this.setState({
+            components: result,
+            isFetching: false
+          });
+          this.mount_table();
+        },
+        e => {
+          this.setState({
+            isFetching: false,
+            isError: true
+          });
+        }
+      );
+  }
 
-  render() {
+  mount_table() {
+    var header_params = [];
+    for (var param in this.state.components[0]) {
+      header_params.push(
+        <Table.HeaderCell key={param}>{param}</Table.HeaderCell>
+      );
+    }
     var table_header = (
       <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>
-            Example {this.props.match.params.id}
-          </Table.HeaderCell>
-          <Table.HeaderCell>{this.props.match.params.id}</Table.HeaderCell>
-          <Table.HeaderCell>
-            Header {this.props.match.params.id}
-          </Table.HeaderCell>
-        </Table.Row>
+        <Table.Row>{header_params}</Table.Row>
       </Table.Header>
     );
-    var table_row = (
-      <Table.Row>
-        {this.table_cell(1)}
-        {this.table_cell(2)}
-        {this.table_cell(3)}
-      </Table.Row>
-    );
 
-    return (
-      <div>
-        <Table celled>
-          {table_header}
-          <Table.Body>
-            {table_row}
-            {table_row}
-            {table_row}
-          </Table.Body>
+    var table_rows = [];
+    this.state.components.forEach((comp, cindex) => {
+      var row_params = [
+        comp.number,
+        comp.code,
+        //comp.description,
+        comp.availability,
+        comp.price
+      ];
+      var all_rows = [];
+      row_params.forEach((param, pindex) => {
+        all_rows.push(<Table.Cell key={pindex}>{param}</Table.Cell>);
+      });
+      table_rows.push(<Table.Row key={cindex}>{all_rows}</Table.Row>);
+    });
 
-          <Table.Footer>
-            <Table.Row>
-              <Table.HeaderCell colSpan="3">
-                <Menu floated="right" pagination>
-                  <Menu.Item as="a" icon>
-                    <Icon name="chevron left" />
-                  </Menu.Item>
-                  <Menu.Item as="a">1</Menu.Item>
-                  <Menu.Item as="a">2</Menu.Item>
-                  <Menu.Item as="a">3</Menu.Item>
-                  <Menu.Item as="a">4</Menu.Item>
-                  <Menu.Item as="a" icon>
-                    <Icon name="chevron right" />
-                  </Menu.Item>
-                </Menu>
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Footer>
-        </Table>
-      </div>
+    this.setState({
+      table_header: table_header,
+      table_rows: table_rows
+    });
+  }
+
+  render() {
+    return this.state.isFetching ? (
+      <Dimmer active>
+        <Loader indeterminate inverted>
+          Preparing Files
+        </Loader>
+      </Dimmer>
+    ) : (
+      <Table celled>
+        {this.state.table_header}
+        <Table.Body>{this.state.table_rows}</Table.Body>
+
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell colSpan="3">
+              <Menu floated="right" pagination>
+                <Menu.Item as="a" icon>
+                  <Icon name="chevron left" />
+                </Menu.Item>
+                <Menu.Item as="a">1</Menu.Item>
+                <Menu.Item as="a">2</Menu.Item>
+                <Menu.Item as="a">3</Menu.Item>
+                <Menu.Item as="a">4</Menu.Item>
+                <Menu.Item as="a" icon>
+                  <Icon name="chevron right" />
+                </Menu.Item>
+              </Menu>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
     );
   }
 }
--- server.js
+++ server.js
@@ -7,4 +7,38 @@ app.get("/api/hello", (req, res) => {
   res.send({ express: "Hello From Express" });
 });
 
+app.get("/api/table/:id", (req, res) => {
+  const response =
+    req.params.id == 2
+      ? 2
+      : [
+          {
+            number: "RASPBERRY-MODB+-512M",
+            code: 2456986,
+            description: {
+              text:
+                "RASPBERRY-PI - Single Board Computer, Raspberry Pi Model B+, BCM2835 CPU, Plugs Into TV and Keyboard, 512MB SDRAM",
+              silicon_manufacturer: "Broadcom",
+              silicon_family_name: "BCM2xxx",
+              core_architecture: "ARM"
+            },
+            availability: 2790,
+            price: 21.29
+          },
+          {
+            number: "RASPBERRY-MODB+-512M",
+            code: 2431426,
+            description: {
+              text: "RASPBERRY-PI - RASPBERRY PI MODEL B+ BOARD",
+              silicon_manufacturer: "Broadcom",
+              silicon_family_name: "BCM2xxx",
+              core_architecture: "ARM"
+            },
+            availability: 0,
+            price: undefined
+          }
+        ];
+  res.send(response);
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
