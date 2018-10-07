351ce78766b8b548d3914fb84772c633046f67ce
Diogo Cruz <diogohmcruz@hotmail.com>
2018-10-07 15:18:04 +0100

    Menu is an Accordion for Search
    The Menu is going to have a Search form.
    This prepares the structure.

--- client/src/Menu.js
+++ client/src/Menu.js
@@ -1,18 +1,43 @@
 import React, { Component } from "react";
-import { Link, Route, Switch } from "react-router-dom";
-import "./App.css";
-import routes from "./routes";
-import ErrorBoundary from "./ErrorBoundary.js";
-import { Grid, Menu, Segment, Sidebar } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Accordion, Grid, Icon, Menu } from "semantic-ui-react";
 
 class HWMenu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeIndex: 0
+    };
+  }
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
+  };
+
   render() {
+    const { activeIndex } = this.state;
+
     return (
       <Grid key={2}>
         <Grid.Column width={3}>
-          <Menu borderless fluid vertical tabular>
-            <Menu.Item as={Link} to="/inventory">
-              Inventory
+          <Accordion as={Menu} fluid vertical>
+            <Menu.Item>
+              <Accordion.Title
+                active={activeIndex === 3}
+                index={3}
+                onClick={this.handleClick}
+              >
+                Inventory
+                <Icon name="dropdown" />
+              </Accordion.Title>
+              <Accordion.Content active={activeIndex === 3}>
+                Search
+              </Accordion.Content>
             </Menu.Item>
             <Menu.Item as={Link} to="/suppliers">
               Buy Supplies
@@ -20,7 +45,7 @@ class HWMenu extends Component {
             <Menu.Item as={Link} to="/users">
               Manage Users
             </Menu.Item>
-          </Menu>
+          </Accordion>
         </Grid.Column>
 
         <Grid.Column width={10}>{this.props.content}</Grid.Column>
