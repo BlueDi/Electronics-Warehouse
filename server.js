a10506bc490ff27e1f069bd6bf9eef39979d6448
Almeida-Oco <intjalmeida@gmail.com>
2018-10-10 16:50:09 +0100

    Main page showing smth

--- server.js
+++ server.js
@@ -1,56 +1,52 @@
-const express = require("express");
-var pgp = require('pg-promise')
-
-var db = pgp('postgres://postgres:password@127.0.0.1:5432/postgres');
+const express = require('express');
+var pgp = require('pg-promise')();
 
 const app = express();
 const port = process.env.PORT || 5000;
+var db = pgp('postgres://postgres:password@127.0.0.1:5432/postgres');
+
+const cat_query = 'SELECT * FROM category;';
+const items_query = 'SELECT item.nome, item.reference, item.condition, item.details, item.manufacturer, item.count FROM item;'
+
+app.get("/api/hello/", (req, res) => {
+  console.log('Hello!')
+  res.send('Hello!');
+});
 
 app.get("/api/category/", (req, res) => {
-  db.any('SELECT * FROM category;')
+  console.log('Category');
+  db.any(cat_query)
     .then(function (data) {
       console.log(data);
       res.send(data);
     })
     .catch (function (error) {
-      console.log('Error! ', error);
-      res.send('Failed to retrieve!');
+      console.log('Error retrieving categories! ', error);
+      res.send('Failed to retrieve categories!');
     });
   res.send({ express: "Hello From Express" });
 });
 
 app.get("/api/table/:id", (req, res) => {
-  const response =
-    req.params.id == 2
-      ? 2
-      : [
-          {
-            number: "RASPBERRY-MODB+-512M",
-            code: 2456986,
-            description: {
-              text:
-                "RASPBERRY-PI - Single Board Computer, Raspberry Pi Model B+, BCM2835 CPU, Plugs Into TV and Keyboard, 512MB SDRAM",
-              silicon_manufacturer: "Broadcom",
-              silicon_family_name: "BCM2xxx",
-              core_architecture: "ARM"
-            },
-            availability: 2790,
-            price: 21.29
-          },
-          {
-            number: "RASPBERRY-MODB+-512M",
-            code: 2431426,
-            description: {
-              text: "RASPBERRY-PI - RASPBERRY PI MODEL B+ BOARD",
-              silicon_manufacturer: "Broadcom",
-              silicon_family_name: "BCM2xxx",
-              core_architecture: "ARM"
-            },
-            availability: 0,
-            price: null
-          }
-        ];
-  res.send(response);
+  console.log('TABLE! -> ID = ', req.params.uid);
+  db.any(items_query)
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        data[i]['details'] = {
+          'details': data[i]['details'],
+          'manufacturer': data[i]['manufacturer'],
+          'condition': data[i]['condition']
+        };
+        delete data[i]['manufacturer'];
+        delete data[i]['condition'];
+      }
+      console.log(data);
+      res.send(data);
+    })
+    .catch (function (error) {
+      console.log('Error retrieving items! ', error);
+      res.send('Failed to retrieve items!');
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
