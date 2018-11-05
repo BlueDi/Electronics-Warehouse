2d4a607cbd7122b920e4718ba4770bbc4e5d9b32
Diogo Cruz <diogohmcruz@hotmail.com>
2018-11-05 16:35:23 +0000

    Resolve "Re-order columns"

--- src/app/pages/table/Table.js
+++ src/app/pages/table/Table.js
@@ -1,97 +1,153 @@
 import React, { Component } from 'react';
-import _ from 'lodash';
-import { Table } from 'semantic-ui-react';
-import GeneralParam from './GeneralParam';
+import { Route } from 'react-router-dom';
+import {
+  PagingState,
+  IntegratedPaging,
+  SearchState,
+  IntegratedFiltering,
+  SortingState,
+  IntegratedSorting,
+  DataTypeProvider
+} from '@devexpress/dx-react-grid';
+import {
+  Grid,
+  DragDropProvider,
+  Table,
+  TableHeaderRow,
+  TableColumnReordering,
+  PagingPanel,
+  ColumnChooser,
+  TableColumnVisibility,
+  SearchPanel,
+  Toolbar
+} from '@devexpress/dx-react-grid-material-ui';
+import { Button, Icon, Image } from 'semantic-ui-react';
+
+const SortingIcon = ({ direction }) =>
+  direction === 'asc' ? <Icon name="arrow up" /> : <Icon name="arrow down" />;
+
+const SortLabel = ({ onSort, children, direction }) => {
+  return children.props.children !== 'image' &&
+    children.props.children !== 'details' ? (
+    <Button fluid icon labelPosition="right" onClick={onSort}>
+      {children}
+      {direction && <SortingIcon direction={direction} />}
+    </Button>
+  ) : (
+    <Button fluid>{children}</Button>
+  );
+};
+
+const DetailsFormatter = ({ value }) => {
+  var details_list = [];
+  for (const param in value) {
+    if (value.hasOwnProperty(param)) {
+      details_list.push(<p>{param + ': ' + value[param]}</p>);
+    }
+  }
+  return details_list;
+};
+
+const DetailsTypeProvider = props => (
+  <DataTypeProvider formatterComponent={DetailsFormatter} {...props} />
+);
+
+const ImageFormatter = ({ value }) => (
+  <Image src={`data:image/png;base64,${value}`} size="small" />
+);
+
+const ImageTypeProvider = props => (
+  <DataTypeProvider formatterComponent={ImageFormatter} {...props} />
+);
+
+class TableRow extends Component {
+  render() {
+    return (
+      <Route
+        render={({ history }) => (
+          <Table.Row
+            {...this.props}
+            onClick={() => history.push('/item/' + this.props.tableRow.row.id)}
+          />
+        )}
+      />
+    );
+  }
+}
 
 class ComponentsTable extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      components: this.props.components
+      columns: [
+        { name: 'name', title: 'Name' },
+        { name: 'location', title: 'Location' },
+        { name: 'reference', title: 'Reference' }
+      ],
+      tableColumnExtensions: [{ columnName: 'name', wordWrapEnabled: true }],
+      detailsColumns: ['details'],
+      imageColumns: ['image'],
+      rows: this.props.components
     };
   }
 
   componentDidMount() {
-    this.mount_table();
+    this.mount_header();
   }
-
-  handleSort = clickedColumn => () => {
-    if (clickedColumn === 'image') return;
-
-    const { column, components, direction } = this.state;
-
-    this.setState(
-      column !== clickedColumn
-        ? {
-            column: clickedColumn,
-            components: _.sortBy(components, [clickedColumn]),
-            direction: 'ascending'
-          }
-        : {
-            components: components.reverse(),
-            direction: direction === 'ascending' ? 'descending' : 'ascending'
-          },
-      () => this.mount_table()
-    );
-  };
 
   mount_header() {
     var header_params = [];
-    for (var param in this.state.components[0]) {
-      if (param !== 'id')
-        header_params.push(
-          <Table.HeaderCell
-            key={param}
-            sorted={this.state.column === param ? this.state.direction : null}
-            onClick={this.handleSort(param)}
-          >
-            {param}
-          </Table.HeaderCell>
-        );
+    for (var param in this.state.rows[0]) {
+      if (param !== 'id') header_params.push({ name: param, title: param });
     }
-    return (
-      <Table.Header>
-        <Table.Row>{header_params}</Table.Row>
-      </Table.Header>
-    );
-  }
-
-  mount_rows() {
-    var table_rows = [];
-    for (let comp of this.state.components) {
-      var row_cells = [];
-      for (var param in comp) {
-        if (param !== 'id')
-          row_cells.push(
-            <GeneralParam
-              key={param}
-              id={comp['id']}
-              value={comp[param]}
-              parameter={param}
-            />
-          );
-      }
-      table_rows.push(<Table.Row key={comp.id}>{row_cells}</Table.Row>);
-    }
-    return <Table.Body>{table_rows}</Table.Body>;
-  }
-
-  mount_table() {
-    var table_header = this.mount_header();
-    var table_rows = this.mount_rows();
 
     this.setState({
-      table_header: table_header,
-      table_rows: table_rows
+      columns: header_params
     });
   }
 
   render() {
+    const {
+      rows,
+      columns,
+      tableColumnExtensions,
+      detailsColumns,
+      imageColumns
+    } = this.state;
     return (
-      <Table key={'content'} celled selectable sortable striped>
-        {this.state.table_header}
-        {this.state.table_rows}
-      </Table>
+      <Grid rows={rows} columns={columns}>
+        <PagingState defaultCurrentPage={0} pageSize={2} />
+        <IntegratedPaging />
+        <DragDropProvider />
+        <SearchState />
+        <IntegratedFiltering />
+        <SortingState
+          defaultSorting={[{ columnName: 'name', direction: 'asc' }]}
+        />
+        <IntegratedSorting />
+        <DetailsTypeProvider for={detailsColumns} />
+        <ImageTypeProvider for={imageColumns} />
+        <Table
+          rowComponent={TableRow}
+          columnExtensions={tableColumnExtensions}
+        />
+        <TableColumnReordering
+          defaultOrder={[
+            'name',
+            'image',
+            'location',
+            'reference',
+            'count',
+            'details'
+          ]}
+        />
+        <TableHeaderRow showSortingControls sortLabelComponent={SortLabel} />
+        <TableColumnVisibility defaultHiddenColumnNames={[]} />
+        <Toolbar />
+        <SearchPanel />
+        <ColumnChooser />
+        <PagingPanel />
+      </Grid>
     );
   }
 }
