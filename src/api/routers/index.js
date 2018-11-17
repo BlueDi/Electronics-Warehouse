70f86c4543221e3f272b39813e6ad0036035c8a9
José Cunha <zeluiscunha@outlook.com>
2018-11-17 23:46:44 +0000

    created request router and 2 queries

--- src/api/routers/index.js
+++ src/api/routers/index.js
@@ -2,5 +2,12 @@ import itemRouter from './item';
 import categoryRouter from './category';
 import propertyRouter from './property';
 import loginRouter from './login';
+import requestRouter from './request';
 
-export default [itemRouter, categoryRouter, propertyRouter, loginRouter];
+export default [
+  itemRouter,
+  categoryRouter,
+  propertyRouter,
+  loginRouter,
+  requestRouter
+];
---
+++ src/api/routers/request.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const db = require('@api/db.js');
+
+const requestRouter = express.Router();
+
+const queryRequest = `
+  SELECT *
+  FROM request_workflow
+  WHERE id = $1
+`;
+
+const queryRequestItems = `
+  SELECT *, convert_from(item.image, 'UTF-8') as image
+  FROM item, request_items
+  WHERE request_items.request_id = $1 AND request_items.item_id = item.id
+`;
+
+requestRouter.get('/request/:id', async (req, res) => {
+  try {
+    const data = await db.any(queryRequest, req.params.id);
+    res.send(data);
+  } catch (e) {
+    res.send('Failed to retrieve selected request info!');
+  }
+});
+
+requestRouter.get('/request_items/:id', async (req, res) => {
+  try {
+    const data = await db.any(queryRequestItems, req.params.id);
+    res.send(data);
+  } catch (e) {
+    res.send('Failed to retrieve selected request items!');
+  }
+});
+
+export default requestRouter;
