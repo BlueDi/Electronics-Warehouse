69ea81d1c5539b9dd7307c8ba7e6666584bb5206
Diogo Cruz <diogohmcruz@hotmail.com>
2018-12-03 18:11:02 +0000

    Mock a form submit

--- src/app/pages/header/tests/LoginForm.test.js
+++ src/app/pages/header/tests/LoginForm.test.js
@@ -4,12 +4,29 @@ import { MemoryRouter as Router } from 'react-router-dom';
 import LoginForm from '@pages/header/LoginForm';
 
 describe('<LoginForm />', () => {
+  const component = renderer.create(
+    <Router>
+      <LoginForm />
+    </Router>
+  );
+
   test('Renders properly', () => {
-    const component = renderer.create(
-      <Router>
-        <LoginForm />
-      </Router>
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('Renders error message properly', () => {
+    const instance = component.root;
+    const input_name = instance.find(
+      el => el.type == 'input' && el.props.name == 'name'
     );
+    input_name.props.onChange({
+      target: { value: 'stu' }
+    });
+
+    const form = instance.find(el => el.type == 'form');
+    form.props.onSubmit();
+
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
