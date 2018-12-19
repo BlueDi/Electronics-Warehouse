3196672cef6a2051b129268f6afaf7790e2a4635
Diogo Cruz <diogohmcruz@hotmail.com>
2018-12-19 09:13:10 +0000

    Revert pages/index

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,17 +1,32 @@
 import loadComponent from './load-component';
 
-// define and export targeted async components
-
-export const Home = loadComponent('home');
-export const WHMenu = loadComponent(() => import('./menu'));
-export const Course = loadComponent(() => import('./course'));
-export const WHTable = loadComponent(() => import('./table'));
-export const Request = loadComponent(() => import('./request'));
-export const WHHeader = loadComponent(() => import('./header'));
-export const AddItem = loadComponent(() => import('./addItem'));
-export const InDepthItem = loadComponent(() => import('./inDepthItem'));
-export const RequestsList = loadComponent(() => import('./requests_list'));
-export const RequestsCartList = loadComponent(() => import('./requests_cart'));
+export const AddItem = loadComponent(() =>
+  import(/* webpackChunkName: 'addItem' */ './addItem')
+);
+export const Course = loadComponent(() =>
+  import(/* webpackChunkName: 'course' */ './course')
+);
+export const WHTable = loadComponent(() =>
+  import(/* webpackChunkName: 'table' */ './table')
+);
+export const WHHeader = loadComponent(() =>
+  import(/* webpackChunkName: 'header' */ './header')
+);
+export const WHMenu = loadComponent(() =>
+  import(/* webpackChunkName: 'menu' */ './menu')
+);
+export const InDepthItem = loadComponent(() =>
+  import(/* webpackChunkName: 'inDepthItem' */ './inDepthItem')
+);
+export const Request = loadComponent(() =>
+  import(/* webpackChunkName: 'request' */ './request')
+);
+export const RequestsCartList = loadComponent(() =>
+  import(/* webpackChunkName: 'requests_cart' */ './requests_cart')
+);
+export const RequestsList = loadComponent(() =>
+  import(/* webpackChunkName: 'requests_list' */ './requests_list')
+);
 
 // export Root as default, and also NotFound as named
 export { default } from './root';
