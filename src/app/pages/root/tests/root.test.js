8d06b82163a75802aa4daff49e4cc70668e04057
Diogo Cruz <diogohmcruz@hotmail.com>
2018-12-21 17:17:33 +0000

    Remove root snapshot test

--- src/app/pages/root/tests/root.test.js
+++ src/app/pages/root/tests/root.test.js
@@ -1,20 +1,7 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import { MemoryRouter as Router } from 'react-router-dom';
 import { Layout } from '@pages/root';
 
 describe('Layout', () => {
   it('should be defined', () => {
     expect(Layout).toBeDefined();
-  });
-
-  it('renders properly', () => {
-    const component = renderer.create(
-      <Router>
-        <Layout />
-      </Router>
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
   });
 });
