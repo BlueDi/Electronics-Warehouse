b0894f1d4b462705f37cc1878ef897c4040d1a0c
Diogo Cruz <diogohmcruz@hotmail.com>
2018-12-21 18:44:14 +0000

    Refactor header/logout
    Now the cart and requests are inside a dropdown.
    
    /spend 1h30m

--- src/app/pages/header/tests/Logout.test.js
+++ src/app/pages/header/tests/Logout.test.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { MemoryRouter as Router } from 'react-router-dom';
-import Logout from '@pages/header/Logout';
+import Logout, { UserMenu } from '@pages/header/Logout';
 
-describe('<Logout />', () => {
-  test('Renders properly', () => {
+describe('UserMenu', () => {
+  it('should be defined', () => {
+    expect(UserMenu).toBeDefined();
+  });
+
+  it('renders properly', () => {
+    const component = renderer.create(
+      <Router>
+        <UserMenu userName={'Test'} userCartLength={1} userPath={'/test'} />
+      </Router>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Logout', () => {
+  it('should be defined', () => {
+    expect(Logout).toBeDefined();
+  });
+
+  it('renders properly', () => {
     const component = renderer.create(
       <Router>
         <Logout />
